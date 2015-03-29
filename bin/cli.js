117acf9f8ef5fcf8900ac07d40416254c8032df1
Michael Williams <dinosaur@riseup.net>
2015-03-29 13:33:38 +1300

    implement command-line runner

---
+++ bin/cli.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+var cli = require('commist')()
+var slice = require('sliced')
+
+var lib = require('../lib')
+
+Object.keys(lib).forEach(function (cmdName) {
+  cli.register(cmdName, runner(lib[cmdName]))
+})
+
+var result = cli.parse(process.argv.splice(2))
+
+if (result) {
+  console.log(usage())
+}
+
+function usage () {
+  return [
+    'Usage: sustain [command] [args ...]',
+    '',
+    '  Commands:',
+    '  - init [bitcoin address]'
+  ].join('\n')
+}
+
+function runner (cmd) {
+  return function (args) {
+    args.push(function (err) {
+      if (err) {
+        console.error(err.message)
+        console.log()
+        console.log(usage())
+      }
+    })
+    cmd.apply(this, args)
+  }
+}
